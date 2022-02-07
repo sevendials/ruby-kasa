714dc4f8084342244a75239bfab17942769c8d32
Christopher Jenkins <christj@gmail.com>
2022-02-06 17:44:29 -0700

    initial commit

---
+++ lib/kasa.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "kasa/version"
+
+# Control local Kasa devices
+module Kasa
+  class Error < StandardError; end
+
+  START_KEY = 171
+  # Your code goes here...
+  def initialize; end
+end
