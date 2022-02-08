3f59f61c98e28c0bd8fd08b226b2fabd850ea236
Christopher Jenkins <christj@gmail.com>
2022-02-08 11:36:48 -0700

    sort paths

--- lib/kasa.rb
+++ lib/kasa.rb
@@ -1,12 +1,37 @@
 # frozen_string_literal: true
 
+require "socket"
 require_relative "kasa/version"
 
 # Control local Kasa devices
-module Kasa
-  class Error < StandardError; end
+class Kasa
+  attr_accessor :ip
 
   START_KEY = 171
   # Your code goes here...
-  def initialize; end
+  def initialize(ip)
+    @ip = ip
+  end
+
+  def get_sysinfo
+    my_string = '{"system": {"get_sysinfo": null}}'
+    key = START_KEY
+
+    plainbytes = my_string.unpack("C*").map do |x|
+      key = key ^ x
+      key
+    end
+
+    payload = ([plainbytes.length] + plainbytes).pack("I>C*")
+    x = Socket.tcp(@ip, 9999) do |s|
+      s.write payload
+      s.recv(s.recv(4).unpack1("I>"))
+    end
+    key = START_KEY
+    x.unpack("C*").map do |cypherbyte|
+      plainbyte = key ^ cypherbyte
+      key = cypherbyte
+      plainbyte
+    end.pack("C*")
+  end
 end
--- lib/kasa/version.rb
+++ lib/kasa/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-module Kasa
+class Kasa
   VERSION = "0.1.0"
 end
