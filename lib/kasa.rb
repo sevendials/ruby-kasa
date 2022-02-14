b5e8ef2170b10e1505815bd3c4bb0ae640d01889
Christopher Jenkins <christj@gmail.com>
2022-02-14 07:27:44 -0700

    add network scan

--- lib/kasa.rb
+++ lib/kasa.rb
@@ -9,31 +9,56 @@ require_relative 'kasa/protocol'
 
 # Control local Kasa devices
 class Kasa
-  attr_accessor :ip
+  attr_reader :devices
 
   ON = 1
   OFF = 0
 
-  def initialize(ip)
-    @ip = ip
+  def initialize
+    @devices = {}
   end
 
-  def sysinfo
-    Kasa::Protocol.get(@ip, '/system/get_sysinfo')
+  # Populate devices hash with Kasa devices
+  def refresh
+    threads = []
+    ip_range.each do |ip|
+      threads << Thread.new do
+        @devices[ip.to_s] = sysinfo(ip.to_s)
+      rescue StandardError => e
+      end
+    end
+    threads.each(&:join)
+    nil
   end
 
+  # Generate array of IP addresses from local subnet
+  def ip_range
+    primary_intf = Socket.getifaddrs.detect { |intf| intf.addr.ipv4_private? }
+    cidr = "#{primary_intf.addr.ip_address}/#{primary_intf.netmask.ip_address}"
+    IPAddr.new(cidr).to_range.to_a.map(&:to_s)
+  end
+
+  # Get system information
+  def sysinfo(ip)
+    Kasa::Protocol.get(ip, '/system/get_sysinfo')
+  end
+
+  # Turn on light
   def on
     relay ON
   end
 
+  # Turn off light
   def off
     relay OFF
   end
 
+  # Get brightness
   def brightness
     sysinfo['brightness']
   end
 
+  # Set brightness
   def brightness=(level)
     Kasa::Protocol.get(@ip, '/smartlife.iot.dimmer/set_brightness/brightness', level)
   end
@@ -44,5 +69,3 @@ class Kasa
     Kasa::Protocol.get(@ip, '/system/set_relay_state/state', state)
   end
 end
-# primary_intf = Socket.getifaddrs.detect { |intf| intf.addr.ipv4_private? }
-# IPAddr.new("#{primary_intf.addr.ip_address}/#{primary_intf.netmask.ip_address}").to_range.to_a.each do |ip|
