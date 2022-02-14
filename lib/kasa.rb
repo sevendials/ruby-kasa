0fd900bd3d588d59c304e41c349abe87ce530e14
Christopher Jenkins <christj@gmail.com>
2022-02-14 08:55:58 -0700

    print nothing when device is not found

--- lib/kasa.rb
+++ lib/kasa.rb
@@ -24,8 +24,8 @@ class Kasa
     ip_range.each do |ip|
       threads << Thread.new do
         @devices << Kasa::Device.new(ip)
-      rescue StandardError => e
-        puts e.message
+      rescue StandardError => _e
+        nil
       end
     end
     threads.each(&:join)
