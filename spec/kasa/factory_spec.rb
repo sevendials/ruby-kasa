5ce8d604b5ce1e29a8ebdf2b8f750cb1f063fb7f
sevendials <christj@gmail.com>
2022-02-16 12:16:07 -0700

    override get method for tests (#3)
    * override get method for tests
    
    * capture correct output
    
    * fix metrics
    
    * remove ids

---
+++ spec/kasa/factory_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+RSpec.describe Kasa::Factory do
+  context 'when power strip' do
+    let :device do
+      described_class.new('192.168.1.118')
+    end
+
+    context 'when sysinfo called' do
+      it 'has a version number' do
+        expect(Kasa::VERSION).not_to be nil
+      end
+
+      it 'returns correct model' do
+        expect(device.sysinfo['model']).to eq('HS300(US)')
+      end
+
+      it 'has children' do
+        expect(device.sysinfo['children'].class).to eq(Array)
+      end
+    end
+
+    context 'when device is turned on with valid index' do
+      it 'succeeds' do
+        expect(device.on(1)).to eq({ 'err_code' => 0 })
+      end
+    end
+
+    context 'when device is turned on with invalid index' do
+      it 'succeeds' do
+        expect(device.on(7)).to eq({ 'err_code' => -14, 'err_msg' => 'entry not exist' })
+      end
+    end
+  end
+end
